c60a450f7e4e984bd2e6ca2e15fdb4f45eadc3f8
gbox3dgel3 <gbox3dgel3@gmail.com>
2024-09-20 15:52:24 +0900

    ....

---
+++ day4/ex01.py
@@ -0,0 +1,22 @@
+#%%
+import numpy as np
+print("numpy version : ", np.__version__)
+# %%
+a1 = np.array([1,2,3,4,5,6])
+a2 = np.array([7,8,9,10,11,12,13,14])
+
+print(a1[2:5])
+print(a2[3:6])
+# %%
+a1[2:5] = a2[3:6]
+print(a1)
+# %%
+z1 = np.zeros((3,))
+print(z1)
+
+# %%
+z2= np.zeros((3,) + (2,2))
+print(z2)
+# %%
+print(z2.shape)
+# %%
