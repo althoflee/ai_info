aa0ee5bdd264d6bd8eeb13e3bbd3ef1ed6a9f9ea
gbox3d <gbox3d@gmail.com>
2024-11-07 16:07:09 +0900

    ...

--- day12/ex03.py
+++ day12/ex03.py
@@ -2,20 +2,24 @@
 import folium.plugins
 import pandas as pd
 import folium
+from folium.plugins import HeatMap
+
 #%%
-data = pd.read_csv("..\__datasets__\seoul-metro-2015.logs.csv")
-station_info = pd.read_csv("..\__datasets__\seoul-metro-station-info.csv")
-# %%
+data = pd.read_csv("../__datasets__/seoul-metro-2021.logs.csv")
 data.info()
-# %%
 data.head()
 # %%
 station_sum = data.groupby('station_code').sum()
 print(station_sum)
 # %%
-station_info.info()
+# 필요한 컬럼만 선택하여 인덱스 설정
+station_info = pd.read_csv("../__datasets__/seoul-metro-station-info.csv")
+station_info = station_info[['station.code', 'geo.latitude', 'geo.longitude']]
+station_info.set_index('station.code', inplace=True)
+print(station_info.head())
 # %%
 station_info.head()
+station_info.info()
 
 # %%
 joined_data = station_sum.join(station_info)
@@ -26,13 +30,11 @@ joined_data.head()
 seoul_in = folium.Map(location=[37.55,126.98],zoom_start=12)
 seoul_in
 
-# %%
-from folium.plugins import HeatMap
-
-#%%
-joined_data[['geo.latitude','geo.longitude','people_in']].head()
 #%%
 HeatMap(data=joined_data[['geo.latitude','geo.longitude','people_in']]).add_to(seoul_in)
 #%%
 joined_data.info()
+# %%
+seoul_in
+
 # %%
